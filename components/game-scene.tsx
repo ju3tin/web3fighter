7f0560aba65f7f8527ee934488ebb95698ad2b3f
Justin <ju3tin@hotmail.co.uk>
2026-01-03 16:51:27 +0000

    Rename Fighter1 component to Fighter

--- components/game-scene.tsx
+++ components/game-scene.tsx
@@ -31,7 +31,7 @@ export function GameScene({ player1Position, player2Position }: GameSceneProps) 
       <Arena />
 
       {/* Fighters */}
-      <Fighter1 position={player1Position} color="#ff0000" name="Player 1" isPlayer1={true} />
+      <Fighter position={player1Position} color="#ff0000" name="Player 1" isPlayer1={true} />
       <Fighter position={player2Position} color="#0066ff" name="Player 2" isPlayer1={false} />
 
       {/* Ground Shadows */}
