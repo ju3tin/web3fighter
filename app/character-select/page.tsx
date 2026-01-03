961d6cc6580d5baa031b33384686ee3e55828676
Justin <ju3tin@hotmail.co.uk>
2026-01-03 23:24:00 +0000

    Update character portraits to new image files

--- app/character-select/page.tsx
+++ app/character-select/page.tsx
@@ -11,12 +11,12 @@ interface Character {
 }
 
 const characters: Character[] = [
-  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu-portrait.jpg' },
-  { id: 'ken', name: 'Ken', portrait: '/characters/ken-portrait.jpg' },
+  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png' },
+  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png' },
   { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png' },
-  { id: 'guile', name: 'Guile', portrait: '/characters/guile-portrait.jpg' },
-  { id: 'zangief', name: 'Zangief', portrait: '/characters/zangief-portrait.jpg' },
-  { id: 'blanka', name: 'Blanka', portrait: '/characters/blanka-portrait.jpg' },
+  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png' },
+  { id: 'zangief', name: 'Zangief', portrait: '/characters/Super_Zangief.png' },
+  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png' },
   // Add as many as you need...
 ];
 
