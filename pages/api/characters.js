a44ba0240f1080816839452619cd9e55b1490d9f
Justin <ju3tin@hotmail.co.uk>
2026-01-04 20:10:11 +0000

    Update characters.js

--- pages/api/characters.js
+++ pages/api/characters.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
   { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb' },
   { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/urbanninja.glb' },
   { id: 'anna', name: 'Anna', portrait: '/characters/anna.png' },
-  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png' },
+  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/fighter_action_2.glb' },
     ];
     
     res.status(200).json(characters);
