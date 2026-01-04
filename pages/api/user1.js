8398cf881d180f68a6905406cfe4863d48361395
Justin <ju3tin@hotmail.co.uk>
2026-01-04 00:02:58 +0000

    Update user1.js

--- pages/api/user1.js
+++ pages/api/user1.js
@@ -2,8 +2,14 @@
 export default function handler(req, res) {
   if (req.method === 'GET') {
     const users = [
-      { id: 1, name: 'Alice' },
-      { id: 2, name: 'Bob' }
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png' },
+  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png' },
+  { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png' },
+  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png' },
+  { id: 'zangief', name: 'Zangief', portrait: '/characters/Super_Zangief.png' },
+  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png' },
     ];
     
     res.status(200).json(users);
