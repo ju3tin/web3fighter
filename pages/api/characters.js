4bb11f7167c8e79e5459af139e432cc279570607
Justin <ju3tin@hotmail.co.uk>
2026-01-05 11:10:03 +0000

    Update characters.js

--- pages/api/characters.js
+++ pages/api/characters.js
@@ -2,14 +2,14 @@
 export default function handler(req, res) {
   if (req.method === 'GET') {
     const characters = [
-  { id: 'xiaoyu', name: 'Xiaoyu', portrait: '/characters/xiaoyu.png', model: '/models/black_girl_avatar.glb' },
-  { id: 'julia', name: 'Julia', portrait: '/characters/julia.png', model: '/models/123.glb' },
-  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png', model: '/models/ryu.glb' },
-  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png', model: '/models/ninja2.glb' },
-  { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb' },
-  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/urbanninja.glb' },
-  { id: 'anna', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb' },
-  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/fighter_action_2.glb' },
+  { id: 'xiaoyu', name: 'Xiaoyu', portrait: '/characters/xiaoyu.png', model: '/models/black_girl_avatar.glb', animelist: '/anime/1' },
+  { id: 'julia', name: 'Julia', portrait: '/characters/julia.png', model: '/models/123.glb', animelist: '/anime/1'  },
+  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png', model: '/models/ryu.glb', animelist: '/anime/1'  },
+  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png', model: '/models/ninja2.glb', animelist: '/anime/1'  },
+  { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb', animelist: '/anime/1'  },
+  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/urbanninja.glb', animelist: '/anime/1'  },
+  { id: 'anna', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/1'  },
+  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/fighter_action_2.glb', animelist: '/anime/1'  },
     ];
     
     res.status(200).json(characters);
