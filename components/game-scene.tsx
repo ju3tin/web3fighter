f8ba5ceafca249ebec6b83c1beba27cce6bd0703
Justin <ju3tin@hotmail.co.uk>
2026-01-06 09:48:38 +0000

    Update game-scene.tsx

--- components/game-scene.tsx
+++ components/game-scene.tsx
@@ -33,7 +33,7 @@ export function GameScene({ player1Position, player2Position, p1 }: GameScenePro
       <Arena />
 
       {/* Fighters */}
-      <Fighter1 position={player1Position} color="#ff0000" name="Player 1" isPlayer1={true} modelPath="/models/ninja2.glb"  />
+      <Fighter1 position={player1Position} color="#ff0000" name={p1} isPlayer1={true} modelPath="/models/ninja2.glb"  />
       <Fighter1 position={player2Position} color="#0066ff" name="Player 2" isPlayer1={false} modelPath="/models/urbanninja.glb" />
 
       {/* Ground Shadows */}
