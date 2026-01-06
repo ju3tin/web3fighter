9f7699e228c3a761dba04cd8b05b6979b8a4d4c6
Justin <ju3tin@hotmail.co.uk>
2026-01-06 21:36:01 +0000

    Update characters.js

--- pages/api/characters.js
+++ pages/api/characters.js
@@ -3,13 +3,13 @@ export default function handler(req, res) {
   if (req.method === 'GET') {
     const characters = [
   { id: 'xiaoyu', name: 'Xiaoyu', portrait: '/characters/xiaoyu.png', model: '/models/black_girl_avatar.glb', animelist: '/anime/1.glb' },
-  { id: 'julia', name: 'Julia', portrait: '/characters/julia.png', model: '/models/123.glb', animelist: '/anime/2.glb'  },
-  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png', model: '/models/ryu.glb', animelist: '/anime/3.glb'  },
-  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png', model: '/models/ninja2.glb', animelist: '/anime/4'  },
-  { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb', animelist: '/anime/5.glb'  },
-  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/urbanninja.glb', animelist: '/anime/6.glb'  },
-  { id: 'anna', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/7.glb'  },
-  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/fighter_action_2.glb', animelist: '/anime/8.glb'  },
+  { id: 'julia', name: 'Julia', portrait: '/characters/julia.png', model: '/models/123.glb', animelist: '/anime/1.glb'  },
+  { id: 'ryu', name: 'Ryu', portrait: '/characters/ryu.png', model: '/models/ryu.glb', animelist: '/anime/1.glb'  },
+  { id: 'ken', name: 'Ken', portrait: '/characters/ken.png', model: '/models/ninja2.glb', animelist: '/anime/1.glb'  },
+  { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb', animelist: '/anime/1.glb'  },
+  { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/urbanninja.glb', animelist: '/anime/1.glb'  },
+  { id: 'anna', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/1.glb'  },
+  { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/fighter_action_2.glb', animelist: '/anime/1.glb'  },
     ];
     
     res.status(200).json(characters);
