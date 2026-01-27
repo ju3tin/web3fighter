dc3e35dff25d7e015c6de9bec456a2a3f4bfd834
Justin <ju3tin@hotmail.co.uk>
2026-01-27 12:28:16 +0000

    Update game-scene.tsx

--- components/game-scene.tsx
+++ components/game-scene.tsx
@@ -36,7 +36,7 @@ export function GameScene({ player1Position, player2Position, p1, model, animeli
 
       {/* Fighters */}
       <Fighter1 position={player1Position} color="#ff0000" name={p1} isPlayer1={true} modelPath={model} animationPath={animelist}  />
-      <Fighter1 position={player2Position} color="#0066ff" name="Player 2" isPlayer1={false} modelPath="/anime/1a.glb" />
+      <Fighter1 position={player2Position} color="#0066ff" name="Player 2" isPlayer1={false} modelPath="/anime/1a.glb"  animationPath="/anime/1a.glb" />
 
       {/* Ground Shadows */}
       <ContactShadows position={[0, -0.01, 0]} opacity={0.5} scale={20} blur={2} far={4} />
