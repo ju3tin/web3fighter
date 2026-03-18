e800b98d4b0459c9fa0a7992d332c493a13a702b
ju3tin <ju3tin@hotmail.co.uk>
2026-03-18 16:37:06 +0000

    sdf

--- components/moves-list.tsx
+++ components/moves-list.tsx
@@ -144,7 +144,7 @@ export function MovesList({
         <h2 className="text-xl font-bold text-foreground mb-1">{characterName}</h2>
         <p className="text-sm text-muted-foreground">Move List & Tutorials</p>
         <div style={{ display: "flex", justifyContent: "flex-end", top: "-10px" }}>
-        <Button color="secondary" onClick={() => setOpen(!open)}>Minimize</Button>
+        <Button color="secondary" onClick={() => {handleClick(); setOpen(!open)}}>Minimize</Button>
 </div>
       </div>
 
