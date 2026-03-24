506851f5f7148bdc487bfac1cd2baa772ccb30fb
ju3tin <ju3tin@hotmail.co.uk>
2026-03-24 14:05:21 +0000

    dfg

---
+++ public/three/test/unit/three.addons.unit.js
@@ -0,0 +1,7 @@
+
+//addons/utils
+import './addons/utils/BufferGeometryUtils.tests.js';
+import './addons/math/ColorSpaces.tests.js';
+import './addons/curves/NURBSCurve.tests.js';
+import './addons/loaders/HDRLoader.tests.js';
+import './addons/exporters/USDZExporter.tests.js';
