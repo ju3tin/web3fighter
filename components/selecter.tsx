3b8a5afa5ba5e04b95c7245a7c7d472419eb6584
Justin <ju3tin@hotmail.co.uk>
2026-03-25 11:20:23 +0000

    Update selecter.tsx

--- components/selecter.tsx
+++ components/selecter.tsx
@@ -7,7 +7,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
 import * as THREE from 'three';
 import { useSearchParams } from "next/navigation"
-import TopBar from '@components/TopBar';
+import TopBar from './TopBar';
 
 interface Character {
   id: string;
