f5ab5f4bb67d3520a3f93104b48149da8521ca94
Justin <ju3tin@hotmail.co.uk>
2026-03-26 22:48:52 +0000

    Update TopBar.tsx

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -2,12 +2,20 @@
 'use client'
 import React from 'react';
 import styles from './TopBar.module.css';
+import { usePathname } from "next/navigation";
 
 // Define the types for the props if needed, for now, we'll just assume no props
 const TopBar: React.FC = () => {
+
+  const pathname = usePathname();
+
+  const isHomePage = pathname === "/";
+  
   return (
     <div className={styles.topBar}>
+      {!isHomePage && (
       <button className={styles.backBtn}>Back</button>
+       )}
       <button className={styles.loginBtn}>Login</button>
     </div>
   );
