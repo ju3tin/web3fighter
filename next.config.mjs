57686f5a2c52fb1f578c39e0d91b701565b4a2f5
Justin <ju3tin@hotmail.co.uk>
2026-04-01 21:34:19 +0100

    Add SVGR loader for handling SVG files

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
-}
 
-export default nextConfig
+  webpack(config) {
+    // Find the existing rule that handles SVGs
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    // Exclude SVGs from default handling
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    // Add SVGR loader
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
