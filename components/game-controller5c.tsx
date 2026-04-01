08aa9e54bf4607e35579aa59bf6163fb227f556f
ju3tin <ju3tin@hotmail.co.uk>
2026-04-01 12:25:32 +0100

    sdf

--- components/game-controller5c.tsx
+++ components/game-controller5c.tsx
@@ -251,231 +251,251 @@ const GameController1: React.FC<GameControllerProps> = ({
       <div className="bg-gradient-to-t from-black/25 to-transparent pt-1 pb-3 px-2 pointer-events-auto">
         <div className="flex items-end justify-between h-44">
           <div className="pl-2 pb-2">
-          <svg
-   id="svg2"
-   viewBox="0 0 78 77.999998"
-   version="1.0"
-   width="78"
-   height="78">
-  <circle
-  id="path3221"
-  style={{
-    display: 'inline',
-    fill: '#333333',
-    fillRule: 'evenodd',
-    stroke: '#000000',
-    strokeWidth: 0.999952,
-    strokeLinecap: 'round',
-    strokeLinejoin: 'round',
-  }}
-  transform="scale(-1,1)"
-  cx={-38.9613}
-  cy={38.72736}
-  r={37.906826}
-/>
-  <path
-  id="path3219"
-  display="inline"
-  fill="#262626"
-  fillRule="evenodd"
-  strokeWidth={1.2761}
-  d="m 29.01968,3.00011 c -1.95255,0 -3.54777,1.59504 -3.54777,3.54736 V 25.25403 H 6.72489 c -1.95254,0 -3.50948,1.59503 -3.50948,3.54736 v 19.89326 c 0,1.96509 1.55694,3.56012 3.50948,3.56012 h 18.74702 v 18.6938 c 0,1.96508 1.59522,3.54736 3.54777,3.54736 h 19.90833 c 1.95255,0 3.50948,-1.58228 3.50948,-3.54736 v -18.6938 h 18.73426 c 1.96531,0 3.50948,-1.59503 3.50948,-3.56012 V 28.80139 c 0,-1.95233 -1.54417,-3.54736 -3.50948,-3.54736 H 52.43749 V 6.54747 c 0,-1.95232 -1.55693,-3.54736 -3.50948,-3.54736 z"
-/>
-<ellipse
-  id="path3246"
-  fill="#5a5a5a"
-  fillRule="evenodd"
-  stroke="#454545"
-  strokeWidth={1.39429}
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  cx={38.953426}
-  cy={63.910221}
-  rx={11.309089}
-  ry={11.6584}
-/>
-<ellipse
-  id="path3244-1"
-  display="inline"
-  fill="#5a5a5a"
-  fillRule="evenodd"
-  stroke="#454545"
-  strokeWidth={1.39429}
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  cx={38.953232}
-  cy={13.760545}
-  rx={11.309089}
-  ry={11.6584}
-/><ellipse
-  id="path3250"
-  fill="#5a5a5a"
-  fillRule="evenodd"
-  stroke="#454545"
-  strokeWidth={1.39429}
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  transform="scale(-1)"
-  cx={-14.636654}
-  cy={-38.868961}
-  rx={11.309089}
-  ry={11.6584}
-/><ellipse
-  id="path3248-7"
-  fill="#5a5a5a"
-  fillRule="evenodd"
-  stroke="#454545"
-  strokeWidth={1.39429}
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  transform="scale(-1)"
-  cx={-62.995564}
-  cy={-38.626194}
-  rx={11.309089}
-  ry={11.6584}
-/>
-<path
-  id="path3309"
-  fill="none"
-  stroke="#00ff83"
-  strokeWidth={1.1416}
-  transform="matrix(1.2038977,0,0,1.2390022,-501.97409,-623.49444)"
-  d="m 455.30029,518.14036 -11.97417,0 5.98708,-10.36993 z"
-/><rect
-  id="rect3307"
-  fill="none"
-  stroke="#ff01db"
-  strokeWidth={1.39429}
-  rx={0}
-  ry={0}
-  height={13.347011}
-  width={12.947104}
-  y={32.162693}
-  x={8.1595421}
-/>
+          <svg xmlns="http://www.w3.org/2000/svg" width={128} height={128}>
+    <circle
+      cx={-64.245}
+      cy={64.011}
+      r={62.862}
+      style={{
+        display: "inline",
+        fill: "#333",
+        fillRule: "evenodd",
+        stroke: "#000",
+        strokeWidth: 1.65824,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+      transform="scale(-1 1)"
+    />
+    <path
+      d="M47.759 4.764c-3.238 0-5.884 2.645-5.884 5.883v31.021H10.787c-3.238 0-5.82 2.645-5.82 5.883v32.99c0 3.258 2.582 5.903 5.82 5.903h31.088v31a5.884 5.884 0 0 0 5.884 5.883h33.014c3.238 0 5.82-2.624 5.82-5.883v-31h31.067c3.26 0 5.82-2.645 5.82-5.904V47.55c0-3.237-2.56-5.882-5.82-5.882H86.593V10.647c0-3.238-2.582-5.883-5.82-5.883z"
+      style={{
+        display: "inline",
+        fill: "#262626",
+        fillRule: "evenodd",
+        strokeWidth: 2.11618,
+      }}
+    />
+    <ellipse
+      cx={64.232}
+      cy={105.772}
+      rx={18.754}
+      ry={19.333}
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.31218,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+    />
+    <ellipse
+      cx={64.232}
+      cy={22.608}
+      rx={18.754}
+      ry={19.333}
+      style={{
+        display: "inline",
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.31218,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+    />
+    <ellipse
+      cx={-23.907}
+      cy={-64.246}
+      rx={18.754}
+      ry={19.333}
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.31218,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+      transform="scale(-1)"
+    />
+    <ellipse
+      cx={-104.102}
+      cy={-63.843}
+      rx={18.754}
+      ry={19.333}
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.31218,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+      transform="scale(-1)"
+    />
+    <path
+      d="M76.184 30.439H52.28L64.23 9.132Z"
+      style={{
+        fill: "none",
+        stroke: "#00ff83",
+        strokeWidth: 2.31213,
+      }}
+    />
+    <rect
+      width={21.47}
+      height={22.134}
+      x={13.166}
+      y={53.125}
+      rx={0}
+      ry={0}
+      style={{
+        fill: "none",
+        stroke: "#ff01db",
+        strokeWidth: 2.31218,
+      }}
+    />
+    <ellipse
+      cx={-104.566}
+      cy={-64.19}
+      rx={12.86}
+      ry={13.257}
+      style={{
+        fill: "none",
+        stroke: "red",
+        strokeWidth: 3.37186,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
+      transform="scale(-1)"
+    />
+    <path
+      d="m54.726 96.157 19.357 19.955m0-19.955-19.357 19.955"
+      style={{
+        fill: "none",
+        stroke: "#8243fb",
+        strokeWidth: 2.31218,
+      }}
+    />
+  </svg>
 
-<ellipse
-  id="path3311"
-  fill="none"
-  stroke="#ff0000"
-  strokeWidth={2.0333}
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  transform="scale(-1)"
-  cx={-63.275723}
-  cy={-38.834965}
-  rx={7.754981}
-  ry={7.9945145}
-/>
-
-<path
-  id="path3313"
-  fill="none"
-  stroke="#8243fb"
-  strokeWidth={1.39429}
-  d="m 33.22124,58.11182 11.67259,12.03315 m 0,-12.03315 -11.67259,12.03315"
-/>
-</svg>
             <Dpad onMove={onPlayer1Move} />
           </div>
           <div className="pr-2 pb-2">
 
-          <svg
-    id="svg2"
-    viewBox="0 0 78 77.999998"
-    width="78"
-    height="78"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <defs id="defs1" />
+          <svg xmlns="http://www.w3.org/2000/svg" width={128} height={128}>
     <circle
-      id="path2416"
+      cx={63.763}
+      cy={63.718}
+      r={62.209}
       style={{
         display: "inline",
-        fill: "#333333",
+        fill: "#333",
         fillRule: "evenodd",
-        stroke: "#000000",
-        strokeWidth: 0.999952,
+        stroke: "#000",
+        strokeWidth: 1.64103,
         strokeLinecap: "round",
         strokeLinejoin: "round",
       }}
-      cx="39.139889"
-      cy="39.095585"
-      r="37.906826"
     />
     <path
-      id="rect3214"
-      style={{ display: "inline", fill: "#262626", fillRule: "evenodd" }}
-      d="m 31.18946,10.62125 c -1.53,0 -2.78,1.25 -2.78,2.78 v 14.66 h -14.68 c -1.54,0 -2.76,1.24 -2.76,2.78 v 15.59 c 0,1.54 1.22,2.78 2.76,2.78 h 14.68 v 14.66 c 0,1.54 1.25,2.78 2.78,2.78 h 15.6 c 1.53,0 2.75,-1.24 2.75,-2.78 v -14.66 h 14.69 c 1.53,0 2.74,-1.24 2.74,-2.78 v -15.59 c 0,-1.54 -1.21,-2.78 -2.74,-2.78 h -14.69 v -14.66 c 0,-1.53 -1.22,-2.78 -2.75,-2.78 z"
+      d="M50.715 16.989a4.574 4.574 0 0 0-4.562 4.562V45.61H22.062c-2.528 0-4.53 2.035-4.53 4.562v25.585c0 2.527 2.002 4.562 4.53 4.562h24.091v24.059a4.563 4.563 0 0 0 4.562 4.562h25.602c2.51 0 4.513-2.035 4.513-4.562V80.319h24.107c2.511 0 4.497-2.035 4.497-4.562V50.172c0-2.527-1.986-4.562-4.497-4.562H80.83V21.55c0-2.51-2.003-4.562-4.513-4.562z"
+      style={{
+        display: "inline",
+        fill: "#262626",
+        fillRule: "evenodd",
+        strokeWidth: 1.64111,
+      }}
     />
     <path
-      style={{ fill: "#28170b", strokeWidth: 1.50491 }}
-      id="path1"
-      d="M 73.508215,49.055538 A 24.385,22.507402 0 0 1 66.296986,65.034134 24.385,22.507402 0 0 1 48.928364,71.562222 L 49.123215,49.055538 Z"
+      d="M120.165 80.064a40.018 36.937 0 0 1-11.835 26.222A40.018 36.937 0 0 1 79.827 117l.32-36.936Z"
+      style={{
+        fill: "#28170b",
+        strokeWidth: 2.46972,
+      }}
     />
     <path
-      style={{ fill: "#28170b", strokeWidth: 1.5856 }}
-      id="path1-0"
-      d="M 73.477182,-27.824688 A 25.208963,24.168493 0 0 1 66.022287,-10.66684 25.208963,24.168493 0 0 1 48.066785,-3.6569663 L 48.268219,-27.824688 Z"
-      transform="scale(1,-1)"
+      d="M120.114 45.222a41.37 39.663 0 0 0-12.234-28.158A41.37 39.663 0 0 0 78.413 5.56l.33 39.662Z"
+      style={{
+        fill: "#28170b",
+        strokeWidth: 2.60214,
+      }}
     />
     <path
-      style={{ fill: "#28170b", strokeWidth: 1.58558 }}
-      id="path1-0-9"
-      d="m -3.0522785,-28.267262 a 25.208963,24.168493 0 0 1 -7.4548955,17.157849 25.208963,24.168493 0 0 1 -17.955502,7.0098732 l 0.201434,-24.1677222 z"
-      transform="scale(-1)"
+      d="M4.54 45.948A41.37 39.663 0 0 1 16.772 17.79 41.37 39.663 0 0 1 46.24 6.286l-.33 39.662z"
+      style={{
+        fill: "#28170b",
+        strokeWidth: 2.60211,
+      }}
     />
     <path
-      style={{ fill: "#28170b", strokeWidth: 1.47927 }}
-      id="path1-0-9-9"
-      d="m -4.5426235,51.161034 a 23.561041,22.507401 0 0 1 -6.9675645,15.978594 23.561041,22.507401 0 0 1 -16.781743,6.528088 l 0.188267,-22.506682 z"
-      transform="scale(-1,1)"
+      d="M6.985 83.52a38.666 36.937 0 0 0 11.434 26.222 38.666 36.937 0 0 0 27.541 10.713l-.309-36.936Z"
+      style={{
+        fill: "#28170b",
+        strokeWidth: 2.42764,
+      }}
     />
     <path
-      style={{ fill: "#818181", fillOpacity: 1, strokeWidth: 0.540859 }}
-      d="m 17.29796,14.27199 -0.19543,6.19542 1.75881,-1.807 2.93133,5.16285 1.95423,-2.58142 -3.32218,-4.90471 0.78169,-1.807 z"
-      id="path2"
+      d="m27.918 22.98-.32 10.168 2.885-2.966 4.811 8.473 3.207-4.236-5.452-8.05 1.283-2.965ZM23.76 104.965l-.321-10.167 2.886 2.965 4.811-8.472 3.207 4.236-5.452 8.05 1.283 2.965zM102.443 21.622l-9.566 3.46 3.822 1.585-6.094 7.602 5.121 1.414 5.465-8.04 3.23.095zM97.726 105.968l-9.566-3.46 3.822-1.585-6.095-7.602 5.122-1.414 5.464 8.04 3.23-.095z"
+      style={{
+        fill: "#818181",
+        fillOpacity: 1,
+        strokeWidth: 0.887608,
+      }}
     />
     <path
-      style={{ fill: "#818181", fillOpacity: 1, strokeWidth: 0.540859 }}
-      d="m 14.76425,64.22909 -0.19543,-6.19542 1.75881,1.807 2.93133,-5.16285 1.95423,2.58142 -3.32218,4.90471 0.78169,1.807 z"
-      id="path2-7"
+      d="M79.099 92.922a3.283 3.743 0 0 0-1.246-3.014L65.494 78.796a3.283 3.743 0 0 0-4.058-.006L48.99 89.902a3.283 3.743 0 0 0-1.262 2.933l-.087 25.795a3.283 3.743 0 0 0 3.286 3.757h24.456a3.283 3.743 0 0 0 3.28-3.67z"
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.97356,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
     />
     <path
-      style={{ fill: "#818181", fillOpacity: 1, strokeWidth: 0.540859 }}
-      d="m 62.70957,13.44414 -5.82898,2.10819 2.3291,0.96642 -3.71389,4.63193 3.12091,0.86179 3.32957,-4.8997 1.96796,0.0585 z"
-      id="path2-0"
+      d="M35.584 44.984a3.674 3.726 0 0 1 2.958 1.413l10.906 14.024a3.674 3.726 0 0 1 .006 4.605L38.548 79.148a3.674 3.726 0 0 1-2.879 1.432l-25.318.099a3.674 3.726 0 0 1-3.688-3.728V49.199a3.674 3.726 0 0 1 3.603-3.722z"
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 3.13813,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
     />
     <path
-      style={{ fill: "#818181", fillOpacity: 1, strokeWidth: 0.540859 }}
-      d="m 59.83494,64.84032 -5.82898,-2.10819 2.3291,-0.96642 -3.71389,-4.63193 3.12091,-0.86179 3.32957,4.8997 1.96796,-0.0585 z"
-      id="path2-0-0"
-    />
-
-    {/* Main Gamepad Buttons */}
-    <path
-      id="path3332"
-      style={{ fill: "#5a5a5a", fillRule: "evenodd", stroke: "#454545", strokeLinecap: "round", strokeLinejoin: "round" }}
-      d="m 318.71094,523.29102 a 1.1789336,1.1789336 0 0 0 -0.94922,0.44726 l -3.5,4.4375 a 1.1789336,1.1789336 0 0 0 -0.002,1.45703 l 3.5,4.46875 a 1.1789336,1.1789336 0 0 0 0.92382,0.45313 l 8.125,0.0312 a 1.1789336,1.1789336 0 0 0 1.1836,-1.17969 V 524.625 a 1.1789336,1.1789336 0 0 0 -1.15625,-1.17773 z"
-      transform="matrix(0,1.9345296,-1.6970837,0,936.55342,-559.6654)"
+      d="M91.866 45.288a3.785 3.721 0 0 0-3.048 1.412L77.58 60.706a3.785 3.721 0 0 0-.006 4.599L88.812 79.41a3.785 3.721 0 0 0 2.966 1.43l26.088.098a3.785 3.721 0 0 0 3.8-3.723V49.499a3.785 3.721 0 0 0-3.713-3.718z"
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 3.18344,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
     />
     <path
-      id="path3324"
-      style={{ fill: "#5a5a5a", fillRule: "evenodd", stroke: "#454545", strokeLinecap: "round", strokeLinejoin: "round" }}
-      d="m 318.71094,523.29102 a 1.1789336,1.1789336 0 0 0 -0.94922,0.44726 l -3.5,4.4375 a 1.1789336,1.1789336 0 0 0 -0.002,1.45703 l 3.5,4.46875 a 1.1789336,1.1789336 0 0 0 0.92382,0.45313 l 8.125,0.0312 a 1.1789336,1.1789336 0 0 0 1.1836,-1.17969 V 524.625 a 1.1789336,1.1789336 0 0 0 -1.15625,-1.17773 z"
-      transform="matrix(-1.8987829,0,0,1.9257072,627.13202,-980.02567)"
+      d="M79.35 33.008a3.283 3.743 0 0 1-1.246 3.013l-12.36 11.112a3.283 3.743 0 0 1-4.057.006L49.24 36.028a3.283 3.743 0 0 1-1.262-2.933L47.892 7.3a3.283 3.743 0 0 1 3.286-3.758h24.456a3.283 3.743 0 0 1 3.28 3.67z"
+      style={{
+        fill: "#5a5a5a",
+        fillRule: "evenodd",
+        stroke: "#454545",
+        strokeWidth: 2.97356,
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+      }}
     />
     <path
-      id="path3322"
-      style={{ fill: "#5a5a5a", fillRule: "evenodd", stroke: "#454545", strokeLinecap: "round", strokeLinejoin: "round" }}
-      d="m 318.71094,523.29102 a 1.1789336,1.1789336 0 0 0 -0.94922,0.44726 l -3.5,4.4375 a 1.1789336,1.1789336 0 0 0 -0.002,1.45703 l 3.5,4.46875 a 1.1789336,1.1789336 0 0 0 0.92382,0.45313 l 8.125,0.0312 a 1.1789336,1.1789336 0 0 0 1.1836,-1.17969 V 524.625 a 1.1789336,1.1789336 0 0 0 -1.15625,-1.17773 z"
-      transform="matrix(1.9564857,0,0,1.9232778,-567.28915,-978.56879)"
-    />
-    <path
-      id="path3334"
-      style={{ fill: "#5a5a5a", fillRule: "evenodd", stroke: "#454545", strokeLinecap: "round", strokeLinejoin: "round" }}
-      d="m 318.71094,523.29102 a 1.1789336,1.1789336 0 0 0 -0.94922,0.44726 l -3.5,4.4375 a 1.1789336,1.1789336 0 0 0 -0.002,1.45703 l 3.5,4.46875 a 1.1789336,1.1789336 0 0 0 0.92382,0.45313 l 8.125,0.0312 a 1.1789336,1.1789336 0 0 0 1.1836,-1.17969 V 524.625 a 1.1789336,1.1789336 0 0 0 -1.15625,-1.17773 z"
-      transform="matrix(0,-1.9345296,-1.6970837,0,936.70613,636.9379)"
+      d="m63.637 5.35-9.172 8.72h18.343zM7.701 61.363l8.711-9.18v18.36ZM121.411 62.884l-8.711-9.18v18.36zM63.637 120.406l-9.172-8.718h18.343z"
+      style={{
+        display: "inline",
+        fill: "#3f3f3f",
+        fillRule: "evenodd",
+        strokeWidth: 0.929205,
+      }}
     />
   </svg>
 
