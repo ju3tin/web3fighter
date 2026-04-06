c9e5326293ab2b74975bf30b278ea910627a7080
Justin <ju3tin@hotmail.co.uk>
2026-04-06 17:45:53 +0100

    Add player rotation to GameScene component
    Added player rotation properties to GameSceneProps and updated Fighter components to use these rotations.

--- components/game-scene.tsx
+++ components/game-scene.tsx
@@ -1,18 +1,29 @@
-"use client"
-import { Environment, ContactShadows } from "@react-three/drei"
-import { Fighter } from "./fighter8"
-import { Arena } from "./arena1"
-import { GameCamera } from "./game-camera"
+"use client";
+
+import { Environment, ContactShadows } from "@react-three/drei";
+import { Fighter } from "./fighter8";
+import { Arena } from "./arena1";
+import { GameCamera } from "./game-camera";
 
 interface GameSceneProps {
-  player1Position: [number, number, number]
-  player2Position: [number, number, number]
-  p1: string
-  model: string
-  animelist: string
+  player1Position: [number, number, number];
+  player2Position: [number, number, number];
+  player1Rotation: [number, number, number];   // ← Added
+  player2Rotation: [number, number, number];   // ← Added
+  p1: string;
+  model: string;
+  animelist: string[];                         // Fixed: should be array
 }
 
-export function GameScene({ player1Position, player2Position, p1, model, animelist }: GameSceneProps) {
+export function GameScene({
+  player1Position,
+  player2Position,
+  player1Rotation,
+  player2Rotation,
+  p1,
+  model,
+  animelist,
+}: GameSceneProps) {
   return (
     <>
       {/* Lighting */}
@@ -35,30 +46,37 @@ export function GameScene({ player1Position, player2Position, p1, model, animeli
       {/* Fighters */}
       <Fighter
         position={player1Position}
-        //color="#ff0000"
+        rotation={player1Rotation}           // ← This makes Player 1 face Player 2
         name="Player 1"
         isPlayer1={true}
         modelPath={model}
-        //animationPath={animelist}
+        // animationPath={animelist}        // Uncomment & adjust if your Fighter supports it
         direction="stop"
         action="fightstance"
       />
+
       <Fighter
         position={player2Position}
-        //color="#0066ff"
+        rotation={player2Rotation}           // ← This makes Player 2 face Player 1
         name="Player 2"
         isPlayer1={false}
         modelPath="/anime/1abc.glb"
-        //animationPath="/anime/1a.glb"
+        // animationPath="/anime/1a.glb"
         direction="stop"
         action="fightstance"
       />
 
       {/* Ground Shadows */}
-      <ContactShadows position={[0, -0.01, 0]} opacity={0.5} scale={20} blur={2} far={4} />
+      <ContactShadows
+        position={[0, -0.01, 0]}
+        opacity={0.5}
+        scale={20}
+        blur={2}
+        far={4}
+      />
 
       {/* Environment for reflections */}
       <Environment preset="sunset" />
     </>
-  )
+  );
 }
