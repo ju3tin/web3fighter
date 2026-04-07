ecee1bf44d4fcfdc55a1a368af915432719eb0fd
Justin <ju3tin@hotmail.co.uk>
2026-04-07 16:54:54 +0100

    Update page.tsx

--- app/(first)/page.tsx
+++ app/(first)/page.tsx
@@ -204,6 +204,7 @@ export default function TekkenGame() {
         loop
         playsInline
         className={styles.video}
+        style={{ z-index:"-2", position: "absolute" }}
       >
         <source src="/video/video00.mp4" type="video/mp4" />
         Your browser does not support the video tag.
