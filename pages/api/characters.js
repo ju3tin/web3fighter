4d3c208868a22f6390d34f225766e3c4535c00a1
Justin <ju3tin@hotmail.co.uk>
2026-04-07 14:24:11 +0100

    Update characters.js

--- pages/api/characters.js
+++ pages/api/characters.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
   { id: 'chunli', name: 'Chun-Li', portrait: '/characters/ChunLi.png', model: '/models/chun_li.glb', animelist: '/anime/1.glb'  },
   { id: 'guile', name: 'Guile', portrait: '/characters/Guile.png', model: '/models/muay_thai_fighter1.glb', animelist: '/anime/1.glb'  },
   { id: 'anna', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/1.glb'  },
-  { id: 'xBot', name: 'Anna', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/1.glb'  },
+  { id: 'xBot', name: 'xBot', portrait: '/characters/anna.png', model: '/models/sitting_talking.glb', animelist: '/anime/1.glb'  },
   { id: 'blanka', name: 'Blanka', portrait: '/characters/Super_Blanka.png', model: '/models/street_fighter_blanka.glb', animelist: '/anime/1a.glb'  },
   { id: 'heihachi', name: 'heihachi', portrait: '/characters/heihachi.png', model: '/models/heihachi.glb', animelist: '/anime/1a.glb'  },
   { id: 'urbanninja', name: 'urbanninja', portrait: '/characters/urbanninja.png', model: '/models/urbanninja.glb', animelist: '/anime/1a.glb'  },
